be820f8ce9fc7040267a323abc7ec8c9edfe74b3
billjiang <jrn1012@petrochina.com.cn>
2018-04-18 22:09:47 +0800

    update

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -4,29 +4,40 @@ var $ = Dom7;
 // Theme
 var theme = 'auto';
 if (document.location.search.indexOf('theme=') >= 0) {
-  theme = document.location.search.split('theme=')[1].split('&')[0];
+    theme = document.location.search.split('theme=')[1].split('&')[0];
 }
 
 // Init App
 var app = new Framework7({
-  id: 'io.framework7.testapp',
-  root: '#app',
-  theme: theme,
-  data: function () {
-    return {
-      user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
-    };
-  },
-  methods: {
-    helloWorld: function () {
-      app.dialog.alert('Hello World!');
+    id: 'io.framework7.testapp',
+    root: '#app',
+    theme: theme,
+    data: function () {
+        return {
+            user: {
+                firstName: 'bill',
+                lastName: 'jiang',
+            },
+        };
     },
-  },
-  routes: routes,
-  vi: {
-    placementId: 'pltd4o7ibb9rc653x14',
-  },
+    methods: {
+        helloWorld: function () {
+            app.dialog.alert('Hello World!');
+        },
+        signIn: function () {
+            var $ = this.$;
+            var app = this.$app;
+            var router = this.$router;
+            var username = $('input#demo-username-2').val();
+            var password = $('input#demo-password-2').val();
+            app.dialog.alert('Username: ' + username + '<br>Password: ' + password, function () {
+                app.loginScreen.close();
+                router.back();
+            })
+        }
+    },
+    routes: routes,
+    vi: {
+        placementId: 'pltd4o7ibb9rc653x14',
+    },
 });
